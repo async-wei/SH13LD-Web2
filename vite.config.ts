83c12d5a73f0c484edd9f19728024118f771ca10
ws <wweskuo@gmail.com>
2025-07-27 04:17:33 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/SH13LD-Web/",
   server: {
-    base: "/SH13LD-Web2/",
     host: "::",
     port: 8080,
   },

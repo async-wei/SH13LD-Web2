f81e84cfb94033ccae4f3795fbafceb5497b8f2c
ws <wweskuo@gmail.com>
2025-07-27 02:21:34 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/SH13LD-Web/",
+  base: "/SH13LD-Web2/",
   server: {
     host: "::",
     port: 8080,
